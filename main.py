ad385f8e03b99f71930198eb03df4980c3076696
DanielMoreiraFr <moreiradefranca.daniel@gmail.com>
2026-09-17 09:53:25 -0300

    estilização da saída + contagem de tempo para execução

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flyfood import calc_flyfood
+import time
+from rich import print
 
 def main():
-    rota, custo = calc_flyfood('matrix.txt')
-    print(f'A melhor rota vai ser: {rota}')
-    print(f'O custo da rota vai ser: {custo}')
+    inicio = time.perf_counter()
+    rota, custo = calc_flyfood('matrix4.txt')
+    fim = time.perf_counter()
+    tempo_exec = fim - inicio
+
+    print(f'A melhor rota vai ser: [blue]{rota}[/blue]')
+    print(f'O custo da rota vai ser: [yellow]{custo}[/yellow]')
+    print(f"Tempo de execução: [green]{tempo_exec:.6f}[/green] segundos")
 
 if __name__ == "__main__":
     main()
